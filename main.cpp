ad19b2abed0980470ff2cc769a1944fb6a7b3443
Alannaas <25c21@kjg.lt>
2024-01-22 15:09:19 +0200

    Pirmas commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+const char CDfv[] = "Duomenys.txt";
+const char CRfv[] = "Rezultatai.txt";
+const int CMax = 100;
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
